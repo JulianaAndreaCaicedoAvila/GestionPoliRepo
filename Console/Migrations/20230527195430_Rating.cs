f1af4ed8fd532611dbf8221823ce8908c44fd8ac
Diego Vargas <dvargas@nemedi.com>
2023-05-27 15:22:52 -0500

    Oracle 19c

---
+++ Console/Migrations/20230527195430_Rating.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Console.Migrations
+{
+    /// <inheritdoc />
+    public partial class Rating : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Rating",
+                table: "Blogs",
+                type: "NUMBER(10)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Blogs");
+        }
+    }
+}

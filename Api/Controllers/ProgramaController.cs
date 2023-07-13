93d04977814193861752fa5e8b803e39e3d73367
Juan Diego Vargas <juandiegovargas@outlook.com>
2023-07-13 00:12:54 -0500

    Controladores

---
+++ Api/Controllers/ProgramaController.cs
@@ -0,0 +1,83 @@
+using System.Text;
+using DevExtreme.AspNet.Data;
+using ESAP.Sirecec.Data;
+using ESAP.Sirecec.Data.Api.Authorization;
+using ESAP.Sirecec.Data.Api.Utils;
+using ESAP.Sirecec.Data.Core;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web.Resource;
+using Newtonsoft.Json;
+
+namespace ESAP.Sirecec.Data.Api.Controllers
+{
+	[@Authorize]
+	[ApiController]
+	[Route("programa")]
+	// [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+	// CRUD => CREATE READ UPDATE DELETE
+	public class ProgramaController : ControllerBase
+	{
+		private readonly DataContext _db;
+		private readonly IHttpContextAccessor _context;
+
+		public ProgramaController(DataContext context, IHttpContextAccessor httpContextAccessor)
+		{
+			_db = context;
+			_context = httpContextAccessor;
+		}
+
+		[HttpPost("ed")] // /api/curso/ed => CREATE - UPDATE
+		public ActionResult Edit(Programa item)
+		{
+			var u = User;
+
+			// Registro nuevo: CREATE
+			if (item.Id == 0)
+			{
+				var obj = (Programa)item.CopyTo(new Programa());
+				obj.CreadoPor = 1; // TODO: 202307121748: Obtener el Id del usuario logueado
+				obj.CreadoEl = DateTime.Now;
+				_db.Programa.Add(obj);
+				_db.SaveChanges();
+				return Ok(obj);
+			}
+			else
+			{
+				// Registro existente: EDIT
+				var current = _db.Programa.First(o => o.Id == item.Id);
+				if (current != null)
+				{
+					var final = (Programa)item.CopyTo(current);
+					final.EditadoPor = 1; // TODO: 202307121748: Obtener el Id del usuario logueado
+					final.EditadoEl = DateTime.Now;
+					_db.SaveChanges();
+					return Ok(final);
+				}
+
+				return Ok(item);
+			}
+		}
+
+		[HttpGet("{itemId?}")] // /api/curso/5 => CREATE - 
+		[Authorization.AllowAnonymous]
+		public ActionResult Get(int? itemId = null)
+		{
+			var item = _db.Programa.FirstOrDefault(o => o.Id == itemId);
+			return Ok(item);
+		}
+
+		[HttpPost("dx")] // /api/curso/dx => DevExtreme DataGrid Get
+		public ActionResult GetDx()
+		{
+			StreamReader reader = new StreamReader(Request.Body, Encoding.UTF8);
+			var str = reader.ReadToEndAsync().Result;
+			var opts = JsonConvert.DeserializeObject<LoadOptions>(str);
+			opts.PrimaryKey = new[] { "Id" };
+			var items = _db.Programa.OrderBy(o => o.Nombre).ToList();
+			var loadResult = DataSourceLoader.Load(items, opts);
+			return Ok(loadResult);
+		}
+	}
+}

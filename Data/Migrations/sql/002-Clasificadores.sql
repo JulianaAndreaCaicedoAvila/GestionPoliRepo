f18d97c34e39f6901786b214c71dc9b846252c54
Diego Vargas <dvargas@nemedi.com>
2023-09-29 06:40:46 -0500

    Municipios

--- Data/Migrations/sql/002-Clasificadores.sql
+++ Data/Migrations/sql/002-Clasificadores.sql
@@ -1,26 +1,26 @@
 ﻿CREATE OR REPLACE VIEW "Clasificadores" AS
 SELECT
-	P."TipoId" AS "PadreTipoId",
-	PT."Nombre" AS "PadreTipoNombre",
-	C."PadreId",
-	P."Nombre" AS "PadreNombre",
-	C."TipoId",
-	T."Nombre" AS "TipoNombre",
-	C."Id",
-	C."Nombre",
-	C."Descripcion",
-	C."Orden",
-	(SELECT COUNT(*) FROM "Clasificador" WHERE "PadreId" = C."Id") AS "Hijos",
-	C."Activo",
-	C."CreadoEl",
-	C."CreadoPor",
-	C."EditadoEl",
-	C."EditadoPor"
+P."TipoId" AS "PadreTipoId",
+PT."Nombre" AS "PadreTipoNombre",
+C."PadreId",
+P."Nombre" AS "PadreNombre",
+C."TipoId",
+T."Nombre" AS "TipoNombre",
+C."Id",
+C."Nombre",
+C."Descripcion",
+C."Orden",
+(SELECT COUNT(*) FROM "Clasificador" WHERE "PadreId" = C."Id") AS "Hijos",
+C."Activo",
+C."CreadoEl",
+C."CreadoPor",
+C."EditadoEl",
+C."EditadoPor"
 FROM
-	"Clasificador" C
-	LEFT OUTER JOIN "ClasificadorTipo" T
-	ON C."TipoId" = T."Id"
-	LEFT OUTER JOIN "Clasificador" P
-	ON C."PadreId" = P."Id"
-	LEFT OUTER JOIN "ClasificadorTipo" PT
-	ON PT."Id" = P."TipoId"
+"Clasificador" C
+LEFT OUTER JOIN "ClasificadorTipo" T
+ON C."TipoId" = T."Id"
+LEFT OUTER JOIN "Clasificador" P
+ON C."PadreId" = P."Id"
+LEFT OUTER JOIN "ClasificadorTipo" PT
+ON PT."Id" = P."TipoId"
---
+++ Data/Migrations/sql/009-Municipios.sql
@@ -0,0 +1,17 @@
+﻿CREATE OR REPLACE VIEW "Municipios" AS
+SELECT
+D."PaisId",
+P."Nombre",
+P."Iso1" AS "PaisIso1",
+P."Iso2" AS "PaisIso2",
+M."DepartamentoId",
+D."Nombre" AS "DepartamentoNombre",
+D."Codigo" AS "DepartamentoCodigo",
+M."Id" AS "MunicipioId",
+M."Nombre" AS "MunicipioNombre",
+M."Codigo" AS "MunicipioCodigo"
+FROM "Departamento" D
+LEFT OUTER JOIN "Pais" P
+ON D."PaisId" = P."Id"
+RIGHT OUTER JOIN "Municipio" M
+ON M."DepartamentoId" = D."Id"
